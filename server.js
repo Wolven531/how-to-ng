55886415ca3bb7b00cd88606cae8a92bdfa2154d
wolven531 <anthony.williams.cs@gmail.com>
2021-03-29 17:55:54 -0600

    Clean up process.env access

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 const appName = 'how-to-ng';
 const distDir = 'dist';
+const serverEnvironment = process.env.NODE_ENV !== 'development';
+const serverPort = process.env.PORT || 8080;
 
 const app = express();
 
@@ -16,11 +18,11 @@ app.get('/*', function(req, res, next) {
 	res.sendFile(path.join(__dirname, distDir, appName, 'index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(serverPort);
 
 function requireHttps(req, res, next) {
 	// for Heroku
-	if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== 'development') {
+	if (!req.secure && req.get('x-forwarded-proto') !== 'https' && serverEnvironment) {
 		return res.redirect('https://'.concat(req.headers.host).concat(req.url));
 	}
 
