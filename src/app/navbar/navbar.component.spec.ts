27300973676ae46f8bb962eec05ab6b22754887a
wolven531 <anthony.williams.cs@gmail.com>
2021-04-01 23:23:58 -0600

    Add navbar comp

---
+++ src/app/navbar/navbar.component.spec.ts
@@ -0,0 +1,20 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { NavbarComponent } from './navbar.component'
+
+describe('NavbarComponent', () => {
+	let component: NavbarComponent
+	let fixture: ComponentFixture<NavbarComponent>
+
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [ NavbarComponent ]
+		}).compileComponents()
+
+		fixture = TestBed.createComponent(NavbarComponent)
+		component = fixture.componentInstance
+	})
+
+	it('creates component', () => {
+		expect(component).toBeTruthy()
+	})
+})
