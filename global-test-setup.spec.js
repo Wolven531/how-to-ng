045c41ecce4dabace4c6f67b70c42449b3de938b
wolven531 <anthony.williams.cs@gmail.com>
2021-04-16 23:13:00 -0600

    Mock remaining console methods

--- global-test-setup.spec.js
+++ global-test-setup.spec.js
@@ -1,8 +1,10 @@
 // run before any single it() block
 beforeAll(() => {
-	spyOn(window.console, 'info')
+	spyOn(window.console, 'debug')
 	spyOn(window.console, 'error')
+	spyOn(window.console, 'info')
 	spyOn(window.console, 'log')
+	spyOn(window.console, 'warn')
 })
 
 // run after every single it() block
