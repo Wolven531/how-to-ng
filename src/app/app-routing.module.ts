2b07dff1d55e2d99e487a2bc96d3391159767f4c
wolven531 <anthony.williams.cs@gmail.com>
2021-04-21 23:21:06 -0600

    Hook up 404/Not found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { DashboardComponent } from './dashboard/dashboard.component'
 import { GameComponent } from './game/game.component'
+import { NotFoundComponent } from './not-found/not-found.component'
 
 const routes: Routes = [
 	{
@@ -16,6 +17,10 @@ const routes: Routes = [
 		path: '',
 		redirectTo: '/dashboard',
 		pathMatch: 'full',
+	},
+	{
+		component: NotFoundComponent,
+		path: '**',
 	},
 ]
 
