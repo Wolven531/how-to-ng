1a50a369fd1efbc1f5f90500ae6efc77ce06883c
wolven531 <anthony.williams.cs@gmail.com>
2021-05-25 23:01:06 -0600

    Add unit test for createDb()

--- src/app/in-memory-data.service.spec.ts
+++ src/app/in-memory-data.service.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing'
+import { Observable } from 'rxjs'
+import { HEROES } from 'src/mock-heroes'
 import { InMemoryDataService } from './in-memory-data.service'
 
 
@@ -7,10 +9,25 @@ describe('InMemoryDataService', () => {
 
 	beforeEach(() => {
 		TestBed.configureTestingModule({})
+
 		service = TestBed.inject(InMemoryDataService)
 	})
 
-	it('should be created', () => {
+	it('creates service', () => {
 		expect(service).toBeTruthy()
+	})
+
+	describe('createDb()', () => {
+		let actual: {} | Observable<{}> | Promise<{}>
+
+		beforeEach(() => {
+			actual = service.createDb()
+		})
+
+		it('creates DB properly', () => {
+			expect(actual).toEqual({
+				heroes: HEROES,
+			})
+		})
 	})
 })
