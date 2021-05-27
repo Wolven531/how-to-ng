69deef64fea5f04d2c46ca2cdde28c5fd68a8233
wolven531 <anthony.williams.cs@gmail.com>
2021-05-26 18:35:32 -0600

    Add additional unit test for genId() (non-empty)

--- src/app/in-memory-data.service.spec.ts
+++ src/app/in-memory-data.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing'
 import { Observable } from 'rxjs'
 import { HEROES } from 'src/mock-heroes'
+import { Hero } from './hero/hero.interface'
 import { InMemoryDataService } from './in-memory-data.service'
 
 
@@ -40,6 +41,32 @@ describe('InMemoryDataService', () => {
 
 		it('returns default ID value (of 11)', () => {
 			expect(actual).toBe(11)
+		})
+	})
+
+	describe('genId() w/ non-empty, non-sorted array', () => {
+		const fakeHeroes: Hero[] = [
+			{
+				id: 2,
+				name: 'two',
+			},
+			{
+				id: 7,
+				name: 'seven',
+			},
+			{
+				id: 5,
+				name: 'five',
+			},
+		]
+		let actual: number
+
+		beforeEach(() => {
+			actual = service.genId(fakeHeroes)
+		})
+
+		it('returns ID value equal to highest Hero ID + 1', () => {
+			expect(actual).toBe(8)
 		})
 	})
 })
